d8bfb7e1c52d1965dbe81b29900eb01dc20a2aa7
Temuulen Molomjamts <69692065+Temuulen-M@users.noreply.github.com>
2025-04-22 16:44:11 +0200

    Time: 0 ms (100.00%) | Memory: 18.2 MB (93.28%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,17 +1,11 @@
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        ans = [None] * n
-
-        for i in range(n):
-            num = i + 1
-            if num%3 == 0 and num%5 == 0:
-                ans[i] = "FizzBuzz"
-            elif num%3 == 0:
-                ans[i] = "Fizz"
-            elif num%5 == 0:
-                ans[i] = "Buzz"
-            else:
-                ans[i] = str(num)
-
+    def fizzBuzz(self, n: int):
+        ans = []
+        for i in range(1, n + 1):
+            ans.append(
+                "FizzBuzz" if i % 15 == 0 else
+                "Buzz" if i % 5 == 0 else
+                "Fizz" if i % 3 == 0 else
+                str(i)
+            )
         return ans
-        
