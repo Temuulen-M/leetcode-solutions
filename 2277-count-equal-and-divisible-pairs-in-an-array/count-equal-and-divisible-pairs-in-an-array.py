490bdb02e8cb41f3ecd20f9522f95e5790226ae1
Temuulen Molomjamts <69692065+Temuulen-M@users.noreply.github.com>
2025-04-25 10:08:28 +0200

    Time: 3 ms (99.24%) | Memory: 18 MB (9.13%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -3,15 +3,15 @@ class Solution:
         hashtable = {} # {num : [i, j, ...]}
         count = 0
         
-        for i in range(len(nums)):
-            if nums[i] in hashtable:
+        for i, n in enumerate(nums):
+            if n in hashtable:
                 # For each previous index of the same number
-                for prev_idx in hashtable[nums[i]]:
+                for prev_idx in hashtable[n]:
                     # Check if product of indices is divisible by k
                     if (i * prev_idx) % k == 0:
                         count += 1
-                hashtable[nums[i]].append(i)
+                hashtable[n].append(i)
             else:
-                hashtable[nums[i]] = [i]
+                hashtable[n] = [i]
                 
         return count
