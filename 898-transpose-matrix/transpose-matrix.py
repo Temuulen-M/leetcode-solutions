b207faae5062ff880a9b5bcb1ea43252f09c28bd
Temuulen Molomjamts <69692065+Temuulen-M@users.noreply.github.com>
2025-04-30 12:27:41 +0200

    Time: 0 ms (100.00%) | Memory: 18.5 MB (69.82%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -1,5 +1,9 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    """
+    # mathmatical procedure
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
         rows, cols = len(matrix), len(matrix[0])
         transposed = [[0] * rows for _ in range(cols)]
 
@@ -8,3 +12,4 @@ class Solution:
                 transposed[j][i] = matrix[i][j]
 
         return transposed
+    """
